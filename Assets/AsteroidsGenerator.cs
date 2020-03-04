309f6e730c7c53ae723dbfdac94a73d029e4c38f
Rasengangstarr <davidouthere@msn.com>
2020-03-04 19:03:39 +0000

    initial commit. Asteroid Field, basic camera and basic object placement completed.

---
+++ Assets/AsteroidsGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidsGenerator : MonoBehaviour
+{
+    public GameObject asteroid1;
+    public GameObject asteroid2;
+    
+    
+    void Start()
+    {
+        for (int x = 0; x < 100; x+=10) {
+            for (int z = 0; z < 100; z+=10) {
+
+                var asteroidNumber = Random.Range(0,2);
+            
+                GameObject asteroidPrefab;
+                if (asteroidNumber == 0) {
+                    asteroidPrefab = asteroid1;
+                }
+                else {
+                    asteroidPrefab = asteroid2;
+                }
+
+                var asteroid = Instantiate(asteroidPrefab, new Vector3(Random.Range(x,x+10) , Random.Range(-5, 5), Random.Range(z,z+10)), Quaternion.identity);
+
+                var scaleFactor = Random.Range(50f, 200f);
+
+                asteroid.transform.localScale = new Vector3(scaleFactor + Random.Range(1f, 5f), scaleFactor + Random.Range(1f, 5f), scaleFactor + Random.Range(1f, 5f));
+                asteroid.transform.Rotate(Random.Range(1, 360), Random.Range(1, 360), Random.Range(1, 360));
+            }
+                
+
+        }
+                     
+    }
+
+}
---
+++ Assets/CameraController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Vector2 rotation = new Vector2 (0, 0);
+    public Transform target;
+    public float speed = 0.5f;
+
+    public GameObject placeholderBuilding; 
+
+    public float rotateLerpSpeed = 16;
+
+    public Vector3 offset;
+
+    public float smoothSpeed = 0.125f;
+
+    bool zoomingToObject = false;
+
+    bool focusedOnObject = false;
+
+    void FixedUpdate()
+    {
+        if (zoomingToObject) {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp (transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+            
+             var q = Quaternion.LookRotation(target.position - transform.position);
+             transform.rotation = Quaternion.Lerp(transform.rotation, q, rotateLerpSpeed);
+
+            if (Vector3.Distance(desiredPosition, transform.position) <= target.gameObject.GetComponent<Renderer>().bounds.size.x * 1.5f ) {
+                zoomingToObject = false;
+                focusedOnObject = true;
+            }
+        }
+        
+    }
+  
+    // Update is called once per frame
+    void Update()
+    {
+        if (!zoomingToObject && !focusedOnObject)
+        {
+            if (Input.GetMouseButtonDown(0)){ // if left button pressed...
+                Ray ray = GetComponent<UnityEngine.Camera>().ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit)){
+                    target =  hit.transform;
+                    zoomingToObject = true;
+                    //transform.LookAt(target);
+                }
+            }
+                
+            if (Input.GetMouseButton(1)){ // if right button pressed...
+                rotation.y += Input.GetAxis ("Mouse X");
+                rotation.x += -Input.GetAxis ("Mouse Y");
+                transform.eulerAngles = (Vector2)rotation * speed;
+            }
+            if(Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
+            }
+            if(Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
+            }
+            if(Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(new Vector3(0,0,speed * Time.deltaTime));
+            }
+            if(Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(new Vector3(0,0,-speed * Time.deltaTime));
+            }
+        }
+        else if (focusedOnObject) {
+
+            transform.LookAt(target);
+
+            if(Input.GetKey(KeyCode.A))
+            {
+                 transform.RotateAround (target.position, new Vector3 (0, 1, 0), 1);
+            }
+            if(Input.GetKey(KeyCode.D))
+            {
+                transform.RotateAround (target.position, new Vector3 (0, 1, 0), -1);
+            }
+            if(Input.GetKey(KeyCode.W))
+            {
+                transform.RotateAround (target.position, new Vector3 (0, 0, 1), 1);
+            }
+            
+            if(Input.GetKey(KeyCode.S))
+            {
+                transform.RotateAround (target.position, new Vector3 (0, 0, 1), -1);
+            }
+
+            if(Input.GetKey(KeyCode.Q))
+            {
+                focusedOnObject = false;
+            }
+
+            if (Input.GetMouseButtonDown(0)){ // if left button pressed...
+                Ray ray = GetComponent<UnityEngine.Camera>().ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit)){
+                    var building = Instantiate(placeholderBuilding,new Vector3(hit.point.x,hit.point.y,hit.point.z), Quaternion.identity);
+
+                    Quaternion wantedRotation = Quaternion.LookRotation(hit.normal);
+                    building.transform.rotation = wantedRotation;
+                    building.transform.Rotate(90,90,90);
+                }
+            }
+            
+        }
+    }
+    
+}
