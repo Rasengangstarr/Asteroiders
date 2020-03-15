d0f7eede664b8f0e44b3fe9adee6ea94e4860a95
Rasengangstarr <davidouthere@msn.com>
2020-03-15 21:20:07 +0000

    started work on unit framework

---
+++ Assets/Scripts/EntityController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EntityController : MonoBehaviour
+{
+    //The distance at which a unit approaching this entity should stop
+   public float ApproachBoundary;
+}
---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UnitController : EntityController
+{
+    protected bool canBuild;
+    protected bool canHaul;
+
+    protected float movementSpeed;
+    protected float haulCapacity;
+
+    protected EntityController target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null) {
+            MoveTowardsTarget();
+        }
+    }
+
+    public void AssignNewTarget(EntityController newTarget) {
+        target = newTarget;
+    }
+
+    protected void MoveTowardsTarget() {
+
+        if(Vector3.Distance(other.position, transform.position) > target.ApproachBoundary) {
+            float step = movementSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+        }
+    }
+}
